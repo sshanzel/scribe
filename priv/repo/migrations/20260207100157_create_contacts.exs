4478b4947f8b0801be9c5b342feffb28d631f8e5
Lee Hansel Solevilla <sshanzel@yahoo.com>
2026-02-07 22:13:27 +0400

    fix: migration mismanagement

--- priv/repo/migrations/20260207100157_create_contacts.exs
+++ priv/repo/migrations/20260207100157_create_contacts.exs
@@ -3,12 +3,15 @@ defmodule SocialScribe.Repo.Migrations.CreateContacts do
 
   def change do
     create table(:contacts) do
+      add :user_id, references(:users, on_delete: :delete_all), null: false
       add :name, :string
-      add :email, :string, null: false
+      add :email, :string
 
       timestamps(type: :utc_datetime)
     end
 
-    create unique_index(:contacts, [:email])
+    create index(:contacts, [:user_id])
+    create index(:contacts, [:email])
+    create unique_index(:contacts, [:user_id, :email])
   end
 end
